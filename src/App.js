472319ad11be77531479ac8e98971fc3ef244802
Samhitha1107 <samhithaaram@gmail.com>
2023-05-16 14:44:48 +0530

    Ecommerce website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './Components';
+import products from './assets/products.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"container"}>
+    <main className={"main"}>
+      <h1>
+        E-Commerce in React and SnipCart
+      </h1>
+      <div className={"grid"}>
+        {
+          products.map((product, i) => <Product {...product} key={i}/>)
+        }
+      </div>
+    </main>
+    <div
+        id="snipcart"
+        data-api-key="NjA1YjZjOWUtMjRlMS00Nzg4LTk5OTktMjkxZDVkOTBiYjljNjM4MTk4MjM5MDE4MzcwNzUx" hidden
+    >
     </div>
+  </div>
   );
 }
 
